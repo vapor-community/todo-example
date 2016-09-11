fb7d8ab56d22f408e1417144e01829c230568d58
Logan Wright <logan.william.wright@gmail.com>
2016-09-11 12:47:26 -0400

    move to leaf for demo

--- App/main.swift
+++ App/main.swift
@@ -1,11 +1,23 @@
 import Vapor
 import VaporMySQL
 
+// MARK: Initialize Droplet 
+
 let drop = Droplet(preparations: [Todo.self], providers: [VaporMySQL.Provider.self])
+
+// MARK: Cors Headers
+
 drop.middleware.append(CorsMiddleware())
 
-drop.get { _ in try drop.view.make("welcome.html") }
-drop.get("tests") { _ in try drop.view.make("todo-backend-tests.html") }
+// MARK: Landing Pages
+
+drop.get { _ in try drop.view.make("welcome") }
+drop.get("tests") { _ in try drop.view.make("todo-backend-tests") }
+
+// MARK: /todos/
+
 drop.grouped(TodoURLMiddleware()).resource("todos", TodoController())
+
+// MARK: Serve
 
 drop.serve()
