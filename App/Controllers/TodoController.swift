15261e4600cc158fcd8902a180e467b6e91a0ddb
Logan Wright <logan.william.wright@gmail.com>
2016-09-11 10:52:26 -0400

    removing logs

--- App/Controllers/TodoController.swift
+++ App/Controllers/TodoController.swift
@@ -9,11 +9,6 @@ extension Todo {
     func makeJson(with request: Request) throws -> JSON {
         let node = try makeNode()
         return try node.makeJson(with: request)
-//        
-//        // TODO: Header "Origin"?
-//        guard let host = request.headers["Host"]?.finished(with: "/") else { throw Abort.notFound }
-//        node["url"] = "\(request.uri.scheme)://\(host)todos/\(id)".makeNode()
-//        return try node.converted()
     }
 }
 
@@ -40,13 +35,11 @@ extension Todo {
 final class TodoController: ResourceRepresentable {
 
     func index(request: Request) throws -> ResponseRepresentable {
-        print("\(#function):\n\(request)")
         let json = try Todo.all().map { try $0.makeJson(with: request) }
         return JSON(json)
     }
 
     func create(request: Request) throws -> ResponseRepresentable {
-        print("\(#function):\n\(request)")
         guard
             var todo = try request.json.flatMap({ try Todo(node: $0) })
             else {
@@ -58,51 +51,31 @@ final class TodoController: ResourceRepresentable {
     }
 
     func show(request: Request, todo: Todo) throws -> ResponseRepresentable {
-        print("\(#function):\n\(request)\n\(todo)")
         return try todo.makeJson(with: request)
     }
 
     func delete(request: Request, todo: Todo) throws -> ResponseRepresentable {
-        print("\(#function):\n\(request)\n\(todo)")
         try todo.delete()
         return JSON([:])
     }
 
     func clear(request: Request) throws -> ResponseRepresentable {
-        print("\(#function):\n\(request)")
         try Todo.query().delete()
         return JSON([])
     }
 
     func update(request: Request, todo: Todo) throws -> ResponseRepresentable {
-        print("\(#function):\n\(request)\n\(todo)")
         guard
             var new = try request.json.flatMap({ try Todo(node: $0) })
             else {
                 throw Abort.notFound
             }
-
-        print("orig: \(todo)")
-        print("pre : \(new)")
         new.merge(existing: todo)
-        print("post: \(new)")
-        print("")
-        // delete old to workaround save not updating bug
-        // try new.save()
         try Todo.query().createOrModify(new.makeNode())
-//        guard let db = Todo.database, let msDriver = db.driver as? MySQLDriver else { throw Abort.notFound }
-//        msDriver.database.execute("UPDATE \(Todo.entity) SET ", <#T##values: [NodeRepresentable]##[NodeRepresentable]#>, <#T##on: Connection?##Connection?#>)
-//        var query = Query<Todo>.init(db)
-//        query.action = .modify
-//        
-//        //let query = try Todo.query()
-//        query.sql = .update
-
         return try new.makeJson(with: request)
     }
 
     func replace(request: Request, todo: Todo) throws -> ResponseRepresentable {
-        print("\(#function):\n\(request)\n\(todo)")
         try todo.delete()
         return try create(request: request)
     }
@@ -120,175 +93,6 @@ final class TodoController: ResourceRepresentable {
     )
 
     func makeResource() -> Resource<Todo> {
-        return Resource(
-            index: index,
-            store: create,
-            show: show,
-            replace: replace,
-            modify: update,
-            destroy: delete,
-            clear: clear,
-            aboutItem: nil,
-            aboutMultiple: nil
-        )
-
-        /*
-        return Resource(
-            index: <#T##Resource.Multiple?##Resource.Multiple?##(Request) throws -> ResponseRepresentable#>,
-            store: <#T##Resource.Multiple?##Resource.Multiple?##(Request) throws -> ResponseRepresentable#>,
-            show: <#T##(Request, StringInitializable) throws -> ResponseRepresentable?##(Request, StringInitializable) throws -> ResponseRepresentable?##(Request, StringInitializable) throws -> ResponseRepresentable#>,
-            replace: <#T##(Request, StringInitializable) throws -> ResponseRepresentable?##(Request, StringInitializable) throws -> ResponseRepresentable?##(Request, StringInitializable) throws -> ResponseRepresentable#>,
-            modify: <#T##(Request, StringInitializable) throws -> ResponseRepresentable?##(Request, StringInitializable) throws -> ResponseRepresentable?##(Request, StringInitializable) throws -> ResponseRepresentable#>,
-            destroy: <#T##(Request, StringInitializable) throws -> ResponseRepresentable?##(Request, StringInitializable) throws -> ResponseRepresentable?##(Request, StringInitializable) throws -> ResponseRepresentable#>,
-            clear: <#T##Resource.Multiple?##Resource.Multiple?##(Request) throws -> ResponseRepresentable#>,
-            aboutItem: <#T##(Request, StringInitializable) throws -> ResponseRepresentable?##(Request, StringInitializable) throws -> ResponseRepresentable?##(Request, StringInitializable) throws -> ResponseRepresentable#>,
-            aboutMultiple: <#T##Resource.Multiple?##Resource.Multiple?##(Request) throws -> ResponseRepresentable#>
-        )
-        */
+        return resource
     }
 }
-
-/*
-class TodoController: Controller {
-    typealias Item = Todo
-    
-    private let todoDao:TodoDao?
-
-    required init(application: Application) {
-        do {
-            try todoDao = TodoDaoImpl()
-        }
-        catch {
-            Log.error("Could not initialize Todo Dao")
-            todoDao = nil
-        }
-        Log.info("Todo controller created")
-    }
-
-    func index(_ request: Request) throws -> ResponseRepresentable {
-        if let todoDao = todoDao {
-            if let todos = todoDao.findAllTodos() {
-                return Json.array(todos.map({ (todo:Todo) -> Json in
-                    todo.url = "http://\(request.uri.host!)/todos/\(todo.id!)"
-                    return todo.makeJson()
-                }))
-            } else {
-                throw Abort.internalServerError
-            }
-        } else {
-            throw Abort.internalServerError
-        }
-    }
-
-    func store(_ request: Request) throws -> ResponseRepresentable {
-        guard let title = request.data["title"]?.string else {
-            throw Abort.badRequest
-        }
-        if let todoDao = todoDao {
-            var completed:Bool
-            if request.data["completed"] != nil && request.data["completed"]!.bool != nil {
-                completed = request.data["completed"]!.bool!
-            } else {
-                completed = false
-            }
-            if let todo = todoDao.createTodo(Todo(id: nil, title: title, completed: completed, order: request.data["order"].int)){
-                todo.url = "http://\(request.uri.host!)/todos/\(todo.id!)"
-                return todo
-            } else {
-                throw Abort.internalServerError
-            }
-        } else {
-            throw Abort.internalServerError
-        }
-    }
-
-    /**
-    	Since item is of type Todo,
-    	only instances of todo will be received
-    */
-    func show(_ request: Request, item todo: Todo) throws -> ResponseRepresentable {
-        if let todoDao = todoDao {
-            if let id = todo.id {
-                if let todo = todoDao.getTodoWithId(id) {
-                    todo.url = "http://\(request.uri.host!)/todos/\(todo.id!)"
-                    return todo
-                } else {
-                    throw Abort.notFound
-                }
-            } else {
-                throw Abort.badRequest
-            }
-        } else {
-            throw Abort.internalServerError
-        }
-    }
-
-    func update(_ request: Request, item todo: Todo) throws -> ResponseRepresentable {
-        if let todoDao = todoDao {
-            if let id = todo.id, let title = request.data["title"]?.string, let completed = request.data["completed"]?.bool, let order = request.data["order"]?.int {
-                let todoToUpdate = Todo(id: id, title: title, completed: completed, order: order)
-                if let updatedTodo = todoDao.updateTodo(todoToUpdate) {
-                    updatedTodo.url = "http://\(request.uri.host!)/todos/\(updatedTodo.id!)"
-                    return updatedTodo
-                } else {
-                    throw Abort.notFound
-                }
-            } else {
-                throw Abort.badRequest
-            }
-        } else {
-            throw Abort.internalServerError
-        }
-    }
-    
-    func modify(_ request: Request, item todo: Todo) throws -> ResponseRepresentable {
-        if let todoDao = todoDao {
-            if let id = todo.id {
-                var changes:[String:Any] = [String:Any]()
-                if let title = request.data["title"]?.string {
-                    changes["title"] = title
-                    Log.info("Changing title of todo \(todo.id!) to '\(title)'")
-                }
-                if let completed = request.data["completed"]?.bool {
-                    changes["completed"] = completed
-                }
-                if let order = request.data["order"]?.int {
-                    changes["order"] = order
-                }
-                if let updatedTodo = todoDao.modifyTodoWithId(id, changes: changes) {
-                    updatedTodo.url = "http://\(request.uri.host!)/todos/\(updatedTodo.id!)"
-                    return updatedTodo
-                } else {
-                    throw Abort.notFound
-                }
-            } else {
-                throw Abort.badRequest
-            }
-        } else {
-            throw Abort.internalServerError
-        }
-    }
-
-    func destroy(_ request: Request, item todo: Todo) throws -> ResponseRepresentable {
-        if let todoDao = todoDao {
-            if let id = todo.id {
-                todoDao.deleteTodoWithId(id)
-                return ""
-            } else {
-                throw Abort.notFound
-            }
-        } else {
-            throw Abort.internalServerError
-        }
-    }
-
-    func destroyAll(_ request: Request) throws -> ResponseRepresentable {
-        if let todoDao = todoDao {
-            todoDao.deleteAllTodos()
-            return ""
-        } else {
-            throw Abort.internalServerError
-        }
-    }
-}
-*/
--- App/Models/Todo.swift
+++ App/Models/Todo.swift
@@ -18,7 +18,6 @@ extension Todo: NodeConvertible {
     }
 
     func makeNode() throws -> Node {
-        defer { print("Made node") }
         return try Node(node:
             [
                 "id": id,
@@ -45,81 +44,3 @@ extension Todo: Preparation {
         fatalError("unimplemented")
     }
 }
-
-/*
-import MongoKitten
-*/
-/*
-final class Todo {
-    var id: String?
-    var title: String?
-    var completed: Bool = false
-    var order:Int?
-    var url:String?
-    
-    init(id:String? = nil, title:String? = nil, completed:Bool = false, order:Int? = nil) {
-        self.title = title
-        self.id = id
-        self.completed = completed
-        self.order = order
-    }
-}
-
-/**
-	This allows instances of Todo to be
-	passed into Json arrays and dictionaries
-	as if it were a native JSON type.
-*/
-extension Todo: JsonRepresentable {
-    func makeJson() -> Json {
-        var json = Json(["completed":completed])
-        if let url = url {
-            json["url"] = .string(url)
-        }
-        if let id = id {
-            json["id"] = .string(id)
-        }
-        if let title = title {
-            json["title"] = .string(title)
-        }
-        if let order = order {
-            json["order"] = .number(Double(order))
-        }
-        return json
-    }
-}
-
-/**
-	If a data structure is StringInitializable,
-	it's Type can be passed into type-safe routing handlers.
-*/
-extension Todo: StringInitializable {
-    convenience init?(from string: String) throws {
-        self.init(id: string)
-    }
-}
-
-extension Todo {
-    convenience init?(fromBson bson:Document) throws {
-        let todoId = bson["_id"].string
-        self.init(id:todoId, title: bson["title"].string, completed: bson["completed"].bool, order:bson["order"].int)
-    }
-    
-    func makeBson() -> Document {
-        var bson = [
-            "completed": ~self.completed
-        ] as Document
-        
-        if let title = self.title {
-            bson["title"] = ~title
-        }
-        if let id = self.id {
-            bson["objectID"] = .objectId(try! ObjectId(id))
-        }
-        if let order = self.order {
-            bson["order"] = ~order
-        }
-        return bson
-    }
-}
-*/
