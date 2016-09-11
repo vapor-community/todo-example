62eaca1217a9ef51f59dab4da1d5047a6f5e80db
Logan Wright <logan.william.wright@gmail.com>
2016-09-11 11:10:42 -0400

    some cleanup

--- App/Controllers/TodoController.swift
+++ App/Controllers/TodoController.swift
@@ -4,7 +4,69 @@ import MySQL
 import FluentMySQL
 import Fluent
 
-var count = 0
+final class TodoController: ResourceRepresentable {
+
+    func index(request: Request) throws -> ResponseRepresentable {
+        let json = try Todo.all().map { try $0.makeJson(with: request) }
+        return JSON(json)
+    }
+
+    func create(request: Request) throws -> ResponseRepresentable {
+        var todo = try request.todo()
+        try todo.save()
+        return try todo.makeJson(with: request)
+    }
+
+    func show(request: Request, todo: Todo) throws -> ResponseRepresentable {
+        return try todo.makeJson(with: request)
+    }
+
+    func delete(request: Request, todo: Todo) throws -> ResponseRepresentable {
+        try todo.delete()
+        return JSON([:])
+    }
+
+    func clear(request: Request) throws -> ResponseRepresentable {
+        try Todo.query().delete()
+        return JSON([])
+    }
+
+    func update(request: Request, existing: Todo) throws -> ResponseRepresentable {
+        var new = try request.todo()
+        new.merge(existing: existing)
+        try Todo.query().createOrModify(new.makeNode())
+        return try new.makeJson(with: request)
+    }
+
+    func replace(request: Request, todo: Todo) throws -> ResponseRepresentable {
+        try todo.delete()
+        return try create(request: request)
+    }
+
+    func makeResource() -> Resource<Todo> {
+        return Resource(
+            index: self.index,
+            store: self.create,
+            show: self.show,
+            replace: self.replace,
+            modify: self.update,
+            destroy: self.delete,
+            clear: self.clear,
+            aboutItem: nil,
+            aboutMultiple: nil
+        )
+    }
+}
+
+extension Request {
+    func todo() throws -> Todo {
+        guard let todo = try json.flatMap({ try Todo(node: $0) }) else {
+            throw Abort.notFound
+        }
+        return todo
+    }
+}
+
 extension Todo {
     func makeJson(with request: Request) throws -> JSON {
         let node = try makeNode()
@@ -29,70 +91,5 @@ extension Todo {
         id = id ?? existing.id
         // completed is always self
         order = order ?? existing.order
-    }
-}
-
-final class TodoController: ResourceRepresentable {
-
-    func index(request: Request) throws -> ResponseRepresentable {
-        let json = try Todo.all().map { try $0.makeJson(with: request) }
-        return JSON(json)
-    }
-
-    func create(request: Request) throws -> ResponseRepresentable {
-        guard
-            var todo = try request.json.flatMap({ try Todo(node: $0) })
-            else {
-                throw Abort.notFound
-            }
-
-        try todo.save()
-        return try todo.makeJson(with: request)
-    }
-
-    func show(request: Request, todo: Todo) throws -> ResponseRepresentable {
-        return try todo.makeJson(with: request)
-    }
-
-    func delete(request: Request, todo: Todo) throws -> ResponseRepresentable {
-        try todo.delete()
-        return JSON([:])
-    }
-
-    func clear(request: Request) throws -> ResponseRepresentable {
-        try Todo.query().delete()
-        return JSON([])
-    }
-
-    func update(request: Request, todo: Todo) throws -> ResponseRepresentable {
-        guard
-            var new = try request.json.flatMap({ try Todo(node: $0) })
-            else {
-                throw Abort.notFound
-            }
-        new.merge(existing: todo)
-        try Todo.query().createOrModify(new.makeNode())
-        return try new.makeJson(with: request)
-    }
-
-    func replace(request: Request, todo: Todo) throws -> ResponseRepresentable {
-        try todo.delete()
-        return try create(request: request)
-    }
-
-    lazy var resource: Resource<Todo> = Resource(
-        index: self.index,
-        store: self.create,
-        show: self.show,
-        replace: self.replace,
-        modify: self.update,
-        destroy: self.delete,
-        clear: self.clear,
-        aboutItem: nil,
-        aboutMultiple: nil
-    )
-
-    func makeResource() -> Resource<Todo> {
-        return resource
     }
 }
--- App/main.swift
+++ App/main.swift
@@ -3,46 +3,11 @@ import HTTP
 import VaporMySQL
 import MySQL
 
-let app = Droplet(preparations: [Todo.self], providers: [VaporMySQL.Provider.self])
-app.middleware.append(CorsMiddleware())
+let drop = Droplet(preparations: [Todo.self], providers: [VaporMySQL.Provider.self])
+drop.middleware.append(CorsMiddleware())
 
-/**
-	This first route will return the welcome.html
-	view to any request to the root directory of the website.
+drop.get { _ in try drop.view.make("welcome.html") }
+drop.get("tests") { _ in try drop.view.make("todo-backend-tests.html") }
+drop.resource("todos", TodoController())
 
-	Views referenced with `app.view` are by default assumed
-	to live in <workDir>/Resources/Views/
-
-	You can override the working directory by passing
-	--workDir to the application upon execution.
-*/
-app.get { _ in try app.view.make("welcome.html") }
-app.get("tests") { _ in try app.view.make("todo-backend-tests.html") }
-//print("T: \(type(of: app.database!))")
-app.resource("todos", TodoController())
-/**
-	This will set up the appropriate GET, PUT, and POST
-	routes for basic CRUD operations. Check out the
-	TodoController in App/Controllers to see more.
-
-	Controllers are also type-safe, with their types being
-	defined by which StringInitializable class they choose
-	to receive as parameters to their functions.
-*/
-// app.resource("todos", controller: TodoController.self)
-
-/**
-	Middleware is a great place to filter
-	and modifying incoming requests and outgoing responses.
-
-	Check out the middleware in App/Middelware.
-
-	You can also add middleware to a single route by
-	calling the routes inside of `app.middleware(MiddelwareType) {
-		app.get() { ... }
-	}`
-*/
-// app.middleware.append(CorsMiddleware())
-
-// Print what link to visit for default port
-app.serve()
+drop.serve()
