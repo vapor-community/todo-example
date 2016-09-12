a952274b536af4cc1d436159d234972fe5006bda
Logan Wright <logan.william.wright@gmail.com>
2016-09-12 04:12:44 -0400

    provider updated

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,7 +3,7 @@ import HTTP
 import Vapor
 import VaporMySQL
 
-let drop = Droplet(preparations: [Todo.self], providers: [VaporMySQL.Provider.Temporary.self])
+let drop = Droplet(preparations: [Todo.self], providers: [VaporMySQL.Provider.self])
 
 // MARK: Cors Headers
 
