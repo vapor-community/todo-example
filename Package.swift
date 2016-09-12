55d1e70eb9cd0c1b0cf927f6658e13b9eca4ced3
Logan Wright <logan.william.wright@gmail.com>
2016-09-12 14:10:51 -0400

    specify fluent version to avoid breaking change

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 18),
         .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/vapor/fluent", Version(0,11,1))
     ],
     exclude: [
         "Config",
